c612f43413f35358188a773e0ee356f294922192
bhavesh992 <surtyanibhavesh29@gmail.com>
2024-10-25 18:25:09 +0530

    Added infinite scroll implmentation

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import React, { useCallback, useRef, useState } from 'react';
+import InfiniteScroll from './InfiniteScroll';
+
+function App() {
+  const [data, setData] = useState([]);
+  const controllerRef=useRef(null)
+
+  const getData = useCallback((pageNumber) => {
+    return new Promise(async(resolve,reject)=>{
+      try{
+        if(controllerRef.current) controllerRef.current.abort();
+          controllerRef.current=new AbortController();
+          const response=await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=9&_page=${pageNumber}`)
+          const responseData=await response.json();
+          setData(prevData=>[...prevData,...responseData]);
+          resolve();
+      }
+      catch (e){
+        console.log(
+          e
+        )
+        reject();
+      }
+    })
+  })
+
+  const renderItemList=useCallback(({title},key,ref)=>{
+   return <div ref={ref} key={key}>{title}</div>
+  },[])
+  return (
+    <>
+      <InfiniteScroll
+        renderItemList={renderItemList}
+        getData={getData}
+        listData={data}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/InfiniteScroll.js
@@ -0,0 +1,40 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+
+const InfiniteScroll = (props) => {
+    const { renderItemList, listData, getData } = props;
+    const [pageNumber, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const observer = useRef(null);
+    const lastElementObserver = useCallback((node) => {
+        if (loading) return;
+        if (observer.current) observer.current.disconnect();
+
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                setPage(prev => prev + 1);
+            }
+        })
+        if (node) observer.current.observe(node)
+    })
+    useEffect(() => {
+        setLoading(true);
+        getData(pageNumber).finally(() => {
+            setLoading(false);
+        })
+    }, [pageNumber])
+
+    const renderList = useCallback(() => {
+        return listData.map((item, index) => {
+            if (index === listData.length - 1) return renderItemList(item, index, lastElementObserver)
+            return renderItemList(item, index, null)
+        })
+    })
+    return (
+        <>
+            {renderList()}
+            {loading && "Loading..."}
+        </>
+    )
+}
+
+export default InfiniteScroll;
